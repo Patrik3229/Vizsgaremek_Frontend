de93d65532884ab6f8b83bb5e62ff6c8178631f3
Patrik3229 <kovacspatrik130@gmail.com>
2024-03-04 20:44:56 +0100

    Sidebar

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,47 +1,56 @@
 export function Sidebar() {
     return <div className="wrapper d-flex align-items-stretch">
-    <nav id="sidebar">
-      <div className="p-4">
+    <nav id="sidebar" className="p-4">
+      <div>
         <h2>
           <a href="index.html" className="logo noUnderline">
             Website name
           </a>
         </h2>
-        <ul className="list-unstyled components">
-          <li className="active">
+        <ul className="list-unstyled">
+          <li>
             <a href="#" className="noUnderline">
-              <span className="fa fa-home mr-3" /> Home
+              <div className="row">
+                <div className="col-2"><span className="fa fa-home mr-3" /></div>
+                <div className="col-10">Home</div>
+              </div>
             </a>
           </li>
           <li>
             <a href="#" className="noUnderline">
-              <span className="fa fa-user mr-3" /> About Us
+              <div className="row">
+                <div className="col-2"><span className="fa fa-gear mr-3" /></div>
+                <div className="col-10">Profile</div>
+              </div>
             </a>
           </li>
           <li>
             <a href="#" className="noUnderline">
-              <span className="fa fa-briefcase mr-3" /> Portfolio
+              <div className="row">
+                <div className="col-2"><span className="fa fa-users mr-3" /></div>
+                <div className="col-10">Users</div>
+              </div>
             </a>
           </li>
           <li>
             <a href="#" className="noUnderline">
-              <span className="fa fa-sticky-note mr-3" /> Blog
-            </a>
-          </li>
-          <li>
-            <a href="#" className="noUnderline">
-              <span className="fa fa-paper-plane mr-3" /> Contact
+              <div className="row">
+                <div className="col-2"><span className="fa fa-info-circle mr-3" /></div>
+                <div className="col-10">About Us</div>
+              </div>
             </a>
           </li>
         </ul>
+      </div>
+      <div>
         <div className="footer">
-          <div className="my-3">
+          <div className="my-2">
             <button className="btn btn-primary navbarbutton">Create a new recipe</button>
           </div>
-          <div className="my-3">
+          <div className="my-2">
             <button className="btn btn-primary navbarbutton">Login</button>
           </div>
-          <div className="my-3">
+          <div className="my-2">
             <button className="btn btn-primary navbarbutton">Register</button>
           </div>
         </div>

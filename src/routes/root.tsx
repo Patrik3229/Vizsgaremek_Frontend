86f7e68e6d58e970e3158adcecead7ea80c3b9d4
Patrik3229 <145651273+Patrik3229@users.noreply.github.com>
2024-03-06 18:35:38 +0100

    Style fix

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,12 +3,12 @@ import { MainPage } from "../Components/MainPage";
 import { Search } from "../Components/Search";
 import { Sidebar } from "../Components/Sidebar";
 import { TopRecipes } from "../Components/TopRecipes";
-import '../css/Root.scoped.css'
+import '../css/Root.scoped.css';
 
 export default function Root() {
     return (
       <>
-      <div className="container-fluid" style={{ height: '100vh' }}>
+      <div className="container-fluid" style={{ height: '100vh' }} id="mainpage">
         <div className="row" style={{ height: '100vh' }}>
           <div className="col-2" style={{ height: '100vh' }}>
             <Sidebar />

6c32a729c66ed100074bfe10204f6959e1345551
Patrik3229 <145651273+Patrik3229@users.noreply.github.com>
2024-04-08 12:29:41 +0200

    Search method

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -40,7 +40,7 @@ export function Search() {
         }
 
         const response = await fetch(`http://localhost:3000/recipes/searchContent`, {
-            method: 'GET',
+            method: 'POST',
             headers: {
                 'Content-type': 'application/json',
                 'Accept': 'application/json',

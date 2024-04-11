323f607c7c1e474f808c23250dca4e06c47983b8
Patrik3229 <kovacspatrik130@gmail.com>
2024-04-11 23:47:07 +0200

    Profile rating & pagination

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -7,14 +7,30 @@ import { ApiContext } from "../api";
 import { NeedsRole } from "../Components/auth";
 import { TopRecipes } from "../Components/TopRecipes";
 
+interface Recipe {
+    id: number;
+    title: string;
+    preptime: number;
+    description: string;
+    allergen_ids: string[];
+}
+
+interface RatingMap {
+    [key: number]: string | number; // Depending on how ratings are actually stored, you might choose `string`, `number`, or another type
+}
 
 export default function Profile() {
 
     const { getUserById } = useContext(ApiContext);
-    const {currentUser} = useContext(ApiContext);
+    const { currentUser } = useContext(ApiContext);
     const { id } = useParams<{ id: string }>();
-    const [userProfile, setUserProfile] = useState(null);
+    const [userProfile, setUserProfile] = useState<any>(null);
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [ratings, setRatings] = useState<RatingMap>({});
+    const [currentPage, setCurrentPage] = useState(0);
+    const [itemsPerPage, _setItemsPerPage] = useState(3);
     const [loading, setLoading] = useState(true);
+    const [recipesLoading, setRecipesLoading] = useState(false);
     const [error, setError] = useState('');
     
     const navigate = useNavigate();
@@ -33,10 +49,44 @@ export default function Profile() {
                 setLoading(false);
                 navigate('/');
             });
+
+        setRecipesLoading(true);
+
+        fetch(`http://localhost:3000/recipes/find-user/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                setRecipes(data);
+                setRecipesLoading(false);
+                return data.map((recipe: Recipe) => {
+                    const fetchUrl = `http://localhost:3000/ratings/find${recipe.id}`; // Corrected URL
+                    console.log('Fetching rating for recipe ID:', recipe.id, 'URL:', fetchUrl); // Log the URL and ID
+                    return fetch(fetchUrl)
+                        .then(res => res.json())
+                        .catch(error => {
+                            console.error('Failed to fetch rating for recipe', recipe.id, error);
+                            return { id: recipe.id, rating: "No rating available" };  // Fallback rating
+                        });
+                });
+            })
+            .then(ratingPromises => Promise.all(ratingPromises))
+            .then(ratingsData => {
+                
+                const ratingsMap = ratingsData.reduce((acc, rating, _index) => {
+                    
+                    acc[rating.id] = rating.rating;
+                    return acc;
+                }, {});
+                setRatings(ratingsMap);
+            })
+            .catch(err => {
+                console.error("Failed to fetch recipes or ratings:", err);
+                setError('Failed to load recipes or ratings');
+                setRecipesLoading(false);
+            });
     }, [id, getUserById]);
 
-    if (loading) {
-        return <div>Loading user data...</div>;
+    if (loading || recipesLoading) {
+        return <div>Loading...</div>;
     }
 
     if (error) {
@@ -48,7 +98,14 @@ export default function Profile() {
     }
 
     const isOwnProfile = currentUser?.id === userProfile.id;
-    console.log(isOwnProfile);
+
+    const totalPages = Math.ceil(recipes.length / itemsPerPage);
+    const startIndex = currentPage * itemsPerPage;
+    const currentRecipes = recipes.slice(startIndex, startIndex + itemsPerPage);
+
+    const handleNavigation = (newPage: number) => {
+        setCurrentPage(newPage);
+    }
 
     return <>
         <div className="container-fluid" style={{ height: '100vh' }} id="mainpage">
@@ -60,27 +117,44 @@ export default function Profile() {
                     <h1>User profile: {userProfile.name}</h1>
                     <h4 style={{ marginBottom: '20px' }}>Role: {userProfile.role.charAt(0).toUpperCase() + userProfile.role.slice(1)}</h4>
                     {isOwnProfile ? (
-                        <button className="btn btn-primary w-100">Edit My Profile</button>
+                        <button className="btn btn-primary w-100 mb-3">Edit My Profile</button>
                     ) : (
                         <NeedsRole role='manager'>
                             <button className="btn btn-primary w-100">Edit User</button>
                         </NeedsRole>
                     )}
                     <h4 style={{ marginTop: '50px' }}>Posted recipes:</h4>
-                    <div className="card w-100" style={{ width: "18rem" }}>
-                        <div className="card-body">
-                            <div className="spbw">
-                                <h5 className="card-title">title</h5>
-                                <h5 className="card-title">preptime</h5>
-                            </div>
-                            <h6 className="card-subtitle mb-2">description</h6>
-                            <div className="spbw">
-                                <span>Allergens:</span>
-                                <span className="fa fa-solid fa-star"></span>
+                    {currentRecipes.map(recipe => (
+                        <div className="card w-100 mb-3" key={recipe.id} style={{ width: "18rem" }}>
+                            <div className="card-body">
+                                <div className="spbw">
+                                    <h5 className="card-title">{recipe.title}</h5>
+                                    <h5 className="card-title">{recipe.preptime} minutes</h5>
+                                </div>
+                                <h6 className="card-subtitle mb-4">{recipe.description}</h6>
+                                <div className="spbw">
+                                    <span>Allergens: {recipe.allergen_ids}</span>
+                                    <span><span className="fa fa-solid fa-star"></span> {ratings[recipe.id]} / 5</span>
+                                </div>
                             </div>
                         </div>
+                    ))}
+                    <div className="d-flex justify-content-center">
+                    <ul className="pagination">
+                        <li className={`page-item ${currentPage === 0 ? 'disabled' : ''}`}>
+                            <a className="page-link" href="#" onClick={(e) => { e.preventDefault(); handleNavigation(0); }}>First</a>
+                        </li>
+                        <li className={`page-item ${currentPage === 0 ? 'disabled' : ''}`}>
+                            <a className="page-link" href="#" onClick={(e) => { e.preventDefault(); handleNavigation(currentPage - 1); }}>Previous</a>
+                        </li>
+                        <li className={`page-item ${currentPage >= totalPages - 1 ? 'disabled' : ''}`}>
+                            <a className="page-link" href="#" onClick={(e) => { e.preventDefault(); handleNavigation(currentPage + 1); }}>Next</a>
+                        </li>
+                        <li className={`page-item ${currentPage >= totalPages - 1 ? 'disabled' : ''}`}>
+                            <a className="page-link" href="#" onClick={(e) => { e.preventDefault(); handleNavigation(totalPages - 1); }}>Last</a>
+                        </li>
+                      </ul>
                     </div>
-
                 </div>
                 <div className="col-2">
                     {/* Placeholder */}
